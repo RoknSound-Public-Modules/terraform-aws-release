31bd4db7c14f02cc9205322a9badbc3ca303a6e8
Dave Arnold <dave@roknsound.com>
2024-08-08 15:18:12 -0700

    chore: Update cloning process in data.tf and add local_file resource in outputs.tf

--- data.tf
+++ data.tf
@@ -11,22 +11,6 @@ data "github_branch" "branch" {
   repository = data.github_repository.repo.name
 }
 
-resource "null_resource" "clone" {
-  triggers = {
-    sha = data.github_branch.branch.sha
-  }
-
-  provisioner "local-exec" {
-    command = "rm -rf ${path.module}/${var.repo_name} || echo; git clone ${github_repository.repo.ssh_url} ${path.module}/${var.repo_name}"
-  }
-}
-
-resource "local_file" "foo" {
-  content    = data.github_branch.branch.sha
-  filename   = "${path.module}/${var.repo_name}/.git_sha"
-  depends_on = [null_resource.clone]
-}
-
 # Archive multiple files and exclude file.
 data "archive_file" "release_files" {
   type        = "zip"
--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+resource "null_resource" "clone" {
+  triggers = {
+    sha = data.github_branch.branch.sha
+  }
+
+  provisioner "local-exec" {
+    command = "rm -rf ${path.module}/${var.repo_name} || echo; git clone ${data.github_repository.repo.ssh_url} ${path.module}/${var.repo_name}"
+  }
+}
+
+resource "local_file" "foo" {
+  content    = data.github_branch.branch.sha
+  filename   = "${path.module}/${var.repo_name}/.git_sha"
+  depends_on = [null_resource.clone]
+}
+
 resource "aws_s3_object" "release" {
   bucket = data.aws_s3_bucket.bucket.bucket
   key    = "${var.repo_name}.zip"
